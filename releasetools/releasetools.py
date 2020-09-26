e2c8240ca0fcf67f7f18740f255634a09037a936
Seden Ecem <ttbpccorporation@outlook.com>
2020-09-26 17:47:11 +0000

    a50: Revert releasetools from A51

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,8 +1,5 @@
-#!/bin/env python3
 #
-# Copyright (C) 2009 The Android Open Source Project
-# Copyright (C) 2019 The Mokee Open Source Project
-# Copyright (C) 2019-2020 The LineageOS Open Source Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,23 +14,22 @@
 # limitations under the License.
 
 import common
-import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-  return
+    OTA_InstallEnd(info)
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-  return
+    OTA_InstallEnd(info)
 
 def AddImage(info, basename, dest):
-  name = basename
-  data = info.input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, name, data)
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+    path = "IMAGES/" + basename
+    if path not in info.input_zip.namelist():
+        return
+
+    data = info.input_zip.read(path)
+    common.ZipWriteStr(info.output_zip, basename, data)
+    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
-  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
-  return
+    AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
